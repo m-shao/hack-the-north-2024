551f42fa891db0873e40ffba736420c48cddb40e
bourrasque <stampixel.t@gmail.com>
2024-09-14 03:19:22 -0400

    Image testing

---
+++ backend/detector.py
@@ -0,0 +1,71 @@
+import os
+from openai import OpenAI
+import dotenv
+
+dotenv.load_dotenv(".env")
+openai_key = os.environ.get("OPENAI_API_KEY")
+client = OpenAI(api_key=openai_key)
+
+
+# completion = client.chat.completions.create(
+#     model="gpt-4o-mini",
+#     messages=[
+#         {"role": "system", "content": "You are a helpful assistant."},
+#         {
+#             "role": "user",
+#             "content": "Write a haiku about recursion in programming."
+#         }
+#     ]
+# )
+#
+# print(completion.choices[0].message)
+
+import base64
+import requests
+
+# OpenAI API Key
+api_key = openai_key
+
+# Function to encode the image
+def encode_image(image_path):
+  with open(image_path, "rb") as image_file:
+    return base64.b64encode(image_file.read()).decode('utf-8')
+
+# Path to your image
+image_path = "nature.jpg"
+
+# Getting the base64 string
+base64_image = encode_image(image_path)
+
+headers = {
+  "Content-Type": "application/json",
+  "Authorization": f"Bearer {api_key}"
+}
+
+# gpt-4o-mini --> 14 secs
+# gpt-4-turbo --> 14 secs
+payload = {
+  "model": "gpt-4o-mini",
+  "messages": [
+    {
+      "role": "user",
+      "content": [
+        {
+          "type": "text",
+          "text": "What’s in this image?"
+        },
+        {
+          "type": "image_url",
+          "image_url": {
+            "url": f"data:image/jpeg;base64,{base64_image}"
+          }
+        }
+      ]
+    }
+  ],
+  "max_tokens": 300
+}
+
+response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+
+print(response.json())
