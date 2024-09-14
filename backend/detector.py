666a08b0eefe245fee02665b16961929377a247c
bourrasque <stampixel.t@gmail.com>
2024-09-14 03:29:45 -0400

    Small fixes

--- backend/detector.py
+++ backend/detector.py
@@ -1,30 +1,12 @@
 import os
 from openai import OpenAI
 import dotenv
+import base64
+import requests
 
 dotenv.load_dotenv(".env")
 openai_key = os.environ.get("OPENAI_API_KEY")
 client = OpenAI(api_key=openai_key)
-
-# completion = client.chat.completions.create(
-#     model="gpt-4o-mini",
-#     messages=[
-#         {"role": "system", "content": "You are a helpful assistant."},
-#         {
-#             "role": "user",
-#             "content": "Write a haiku about recursion in programming."
-#         }
-#     ]
-# )
-#
-# print(completion.choices[0].message)
-
-import base64
-import requests
-
-# OpenAI API Key
-api_key = openai_key
-
 
 # Function to encode the image
 def encode_image(image_path):
@@ -33,14 +15,14 @@ def encode_image(image_path):
 
 
 # Path to your image
-image_path = "nature.jpg"
+image_path = "nature.jpg"  # 728 x 410: gpt-4o-mini --> 4 secs
 
 # Getting the base64 string
 base64_image = encode_image(image_path)
 
 headers = {
     "Content-Type": "application/json",
-    "Authorization": f"Bearer {api_key}"
+    "Authorization": f"Bearer {openai_key}"
 }
 
 # gpt-4o-mini --> 14 secs
@@ -48,6 +30,8 @@ headers = {
 payload = {
     "model": "gpt-4o-mini",
     "messages": [
+        {"role": "system",
+         "content": "You are designed to help describe the environments around a visually impaired person."},
         {
             "role": "user",
             "content": [
