4e1036819dc1de72050794de1491e0855afec22b
bourrasque <stampixel.t@gmail.com>
2024-09-14 03:26:30 -0400

    Image testing

--- backend/detector.py
+++ backend/detector.py
@@ -6,7 +6,6 @@ dotenv.load_dotenv(".env")
 openai_key = os.environ.get("OPENAI_API_KEY")
 client = OpenAI(api_key=openai_key)
 
-
 # completion = client.chat.completions.create(
 #     model="gpt-4o-mini",
 #     messages=[
@@ -26,10 +25,12 @@ import requests
 # OpenAI API Key
 api_key = openai_key
 
+
 # Function to encode the image
 def encode_image(image_path):
-  with open(image_path, "rb") as image_file:
-    return base64.b64encode(image_file.read()).decode('utf-8')
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode('utf-8')
+
 
 # Path to your image
 image_path = "nature.jpg"
@@ -38,32 +39,33 @@ image_path = "nature.jpg"
 base64_image = encode_image(image_path)
 
 headers = {
-  "Content-Type": "application/json",
-  "Authorization": f"Bearer {api_key}"
+    "Content-Type": "application/json",
+    "Authorization": f"Bearer {api_key}"
 }
 
 # gpt-4o-mini --> 14 secs
 # gpt-4-turbo --> 14 secs
 payload = {
-  "model": "gpt-4o-mini",
-  "messages": [
-    {
-      "role": "user",
-      "content": [
+    "model": "gpt-4o-mini",
+    "messages": [
         {
-          "type": "text",
-          "text": "What’s in this image?"
-        },
-        {
-          "type": "image_url",
-          "image_url": {
-            "url": f"data:image/jpeg;base64,{base64_image}"
-          }
+            "role": "user",
+            "content": [
+                {
+                    "type": "text",
+                    "text": "I am a visually impaired person, describe the image in a way to help me understand it "
+                            "the best."
+                },
+                {
+                    "type": "image_url",
+                    "image_url": {
+                        "url": f"data:image/jpeg;base64,{base64_image}"
+                    }
+                }
+            ]
         }
-      ]
-    }
-  ],
-  "max_tokens": 300
+    ],
+    "max_tokens": 300
 }
 
 response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
