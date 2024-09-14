2cb15e53d61f2f9d6be33ab80992e6969fc30667
bourrasque <stampixel.t@gmail.com>
2024-09-14 03:42:46 -0400

    Complete image capture

--- backend/detector.py
+++ backend/detector.py
@@ -1,4 +1,5 @@
 import os
+from cv2 import VideoCapture, imshow, imwrite
 from openai import OpenAI
 import dotenv
 import base64
@@ -8,50 +9,64 @@ dotenv.load_dotenv(".env")
 openai_key = os.environ.get("OPENAI_API_KEY")
 client = OpenAI(api_key=openai_key)
 
-# Function to encode the image
-def encode_image(image_path):
-    with open(image_path, "rb") as image_file:
-        return base64.b64encode(image_file.read()).decode('utf-8')
+
+def capture_image():
+    cam = VideoCapture(0)
+
+    result, image = cam.read()
+    if result:
+        # saving image in local storage
+        imwrite("captured_image.png", image)
+    else:
+        print("No image detected. Please! try again")
 
 
-# Path to your image
-image_path = "nature.jpg"  # 728 x 410: gpt-4o-mini --> 4 secs
+def get_image_text():
+    # Function to encode the image
+    def encode_image(image_path):
+        with open(image_path, "rb") as image_file:
+            return base64.b64encode(image_file.read()).decode('utf-8')
 
-# Getting the base64 string
-base64_image = encode_image(image_path)
+    image_path = "captured_image.png"  # 728 x 410: gpt-4o-mini --> 4 secs
+    base64_image = encode_image(image_path)
 
-headers = {
-    "Content-Type": "application/json",
-    "Authorization": f"Bearer {openai_key}"
-}
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {openai_key}"
+    }
 
-# gpt-4o-mini --> 14 secs
-# gpt-4-turbo --> 14 secs
-payload = {
-    "model": "gpt-4o-mini",
-    "messages": [
-        {"role": "system",
-         "content": "You are designed to help describe the environments around a visually impaired person."},
-        {
-            "role": "user",
-            "content": [
-                {
-                    "type": "text",
-                    "text": "I am a visually impaired person, describe the image in a way to help me understand it "
-                            "the best."
-                },
-                {
-                    "type": "image_url",
-                    "image_url": {
-                        "url": f"data:image/jpeg;base64,{base64_image}"
+    # gpt-4o-mini --> 14 secs
+    # gpt-4-turbo --> 14 secs
+    payload = {
+        "model": "gpt-4o-mini",
+        "messages": [
+            {"role": "system",
+             "content": "You are designed to help describe the environments around a visually impaired person."},
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": "I am a visually impaired person, describe the image to me in a way to help me better "
+                                "understand it"
+                                "the best."
+                    },
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": f"data:image/jpeg;base64,{base64_image}"
+                        }
                     }
-                }
-            ]
-        }
-    ],
-    "max_tokens": 300
-}
+                ]
+            }
+        ],
+        "max_tokens": 300
+    }
 
-response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+    response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
 
-print(response.json())
+    print(response.json())
+
+
+capture_image()
+get_image_text()
