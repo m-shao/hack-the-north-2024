02d533c6102ee2d1fb1c3b2c332f1f5d52571574
Gregory <gregory.ovis@gmail.com>
2024-09-15 05:48:31 -0400

    Minor bug fixes

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -10,10 +10,9 @@ import {
   Marker,
 } from "@mappedin/mappedin-js";
 import "@mappedin/mappedin-js/lib/index.css";
-import { db, doc, getDoc,updateDoc } from './firebase'; // Import Firestore references
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlay, faPause } from '@fortawesome/free-solid-svg-icons';
-
+import { db, doc, getDoc, updateDoc } from "./firebase"; // Import Firestore references
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlay, faPause } from "@fortawesome/free-solid-svg-icons";
 
 interface Asset {
   name: string;
@@ -34,54 +33,87 @@ const options = {
 };
 
 const Page = () => {
-  const data = useRef("");
+  const [data, setData] = useState<string | null>(""); // Initialize with null or empty object
   const [lastModified, setLastModified] = useState(null);
   const [error, setError] = useState(null);
 
-  async function updateFirestoreWithInstructions(instructions: TDirectionInstruction[]) {
-	try {
-	  // Get the Firestore document reference
-	  const docRef = doc(db, "HTN2024", "data");
+  async function updateFirestoreWithNearme(
+    nearMe: String[]
+  ){
+    try {
+      // Get the Firestore document reference
+      const docRef = doc(db, "HTN2024", "data");
+      console.log(docRef)
+      console.log("Updating: ", nearMe);
 
-	  // Update the "front to back" field in the document
-	  await updateDoc(docRef, {
-		"front to back": instructions.map((instr) => instr.action.type + " " + (instr.action.bearing || "")) // Adjust as per your instruction structure
-	  });
 
-	  console.log("Firestore document updated with instructions!");
-	} catch (error) {
-	  console.error("Error updating Firestore document: ", error);
-	}
+      // Update the "front to back" field in the document
+      const res = await updateDoc(docRef,{"near me" : nearMe.join(",")});
+
+      console.log(res)
+
+      console.log("Firestore document updated with instructions!");
+    } catch (error) {
+      console.error("Error updating Firestore document: ", error);
+    }
+  }
+
+  async function updateFirestoreWithInstructions(
+    instruction: TDirectionInstruction
+  ) {
+    try {
+      // Get the Firestore document reference
+      const docRef = doc(db, "HTN2024", "data");
+      console.log(docRef)
+      console.log("Updating: ", instruction);
+
+      const distance = 100
+
+      // Update the "front to back" field in the document
+      const res = await updateDoc(docRef, {
+        "front to back": `${instruction.action.type} ${
+          instruction.action.bearing ?? ""
+        } and walk ${Math.round(instruction.distance / 0.7)} steps`, // Adjust as per your instruction structure
+      });
+
+      console.log(res)
+
+      console.log("Firestore document updated with instructions!");
+    } catch (error) {
+      console.error("Error updating Firestore document: ", error);
+    }
   }
 
   useEffect(() => {
-	const checkFirestoreDoc = async () => {
-	  try {
-		const docRef = doc(db, "HTN2024", "data"); // Firestore document reference
-		const docSnap = await getDoc(docRef); // Fetch the document
+    const checkFirestoreDoc = async () => {
+      try {
+        const docRef = doc(db, "HTN2024", "data"); // Firestore document reference
+        const docSnap = await getDoc(docRef); // Fetch the document
 
-		if (docSnap.exists()) {
-		  const jsonData = docSnap.data(); // Document data
-		  const modified = docSnap.updateTime?.toDate().toString(); // Document's last updated time
+        if (docSnap.exists()) {
+          const jsonData = docSnap.data(); // Document data
+          const modified = docSnap.updateTime?.toDate().toString(); // Document's last updated time
 
-		  setLastModified(modified); // Update last modified time
+          setLastModified(modified); // Update last modified time
 
-		  // Check if "back to front" data has changed
-		  if (jsonData["back to front"] !== "" && jsonData["back to front"] !== data.current) {
-			data.current = jsonData["back to front"];
-			console.log("Updated location: ", data.current); // Log the updated data
-		  }
-		}
-	  } catch (err) {
-		setError(err);
-		console.error("Error fetching Firestore document: ", err);
-	  }
-	};
+          // Check if "back to front" data has changed
+          if (
+            jsonData["back to front"] !== "" &&
+            jsonData["back to front"] !== data
+          ) {
+            setData(jsonData["back to front"]);
+          }
+        }
+      } catch (err) {
+        setError(err);
+        console.error("Error fetching Firestore document: ", err);
+      }
+    };
 
-	checkFirestoreDoc(); // Initial check
-	const interval = setInterval(checkFirestoreDoc, 3000); // Poll every 3 seconds
+    checkFirestoreDoc(); // Initial check
+    const interval = setInterval(checkFirestoreDoc, 3000); // Poll every 3 seconds
 
-	return () => clearInterval(interval); // Clean up the interval on component unmount
+    return () => clearInterval(interval); // Clean up the interval on component unmount
   }, [lastModified]); // Dependency array based on last modified
 
   let endPosition;
@@ -137,121 +169,120 @@ const Page = () => {
 
     return Math.round(R * c);
   }
-  async function init() {
-	setTranscription("");  // Reset the transcription state
-	const mapData = await getMapData(options);
-	const mapView = await show3dMap(mapRef.current as HTMLDivElement, mapData);
+  async function init(data) {
+    setTranscription(""); // Reset the transcription state
+    const mapData = await getMapData(options);
+    const mapView = await show3dMap(mapRef.current as HTMLDivElement, mapData);
+    console.log(data);
+    const spaces = await mapData.getByType("space");
+    endPosition =spaces.indexOf(spaces.find((item) => item.name === data));
 
-	const spaces = await mapData.getByType("space");
-	 endPosition = spaces.indexOf(spaces.find((item) => item.name == data.current));
+    await spaces.forEach((space: Space, index: number) => {
+      if (space.name && space.name.trim().length > 0) {
+        mapView.Labels.add(space.center, space.name); // Add label at the space center
+      }
+    });
 
-	await spaces.forEach((space: Space, index: number) => {
-	  if (space.name && space.name.trim().length > 0) {
-		mapView.Labels.add(space.center, space.name); // Add label at the space center
-	  }
-	});
+    const assets: Asset[] = [];
+    function updateAsset(a: Asset, duration: number) {
+      if (!isPause.current) {
+        if (!a.directions || a.directions.distance === 0) {
+          a.destinationSpace = mapData.getByType("space")[endPosition];
+          a.directions = mapView.getDirections(
+            a.originSpace,
+            a.destinationSpace
+          );
+          mapView.Navigation.draw(a.directions!);
+          if (a.directions && a.directions.distance === 0) {
+            return;
+          }
+        }
 
-	const assets: Asset[] = [];
+        const coordinate = a.directions?.coordinates[a.step];
+        const distance = roundDistance(
+          coordinate?.latitude,
+          coordinate?.longitude,
+          a.currentInstruction?.coordinate.latitude,
+          a.currentInstruction?.coordinate.longitude
+        );
 
-	function updateAsset(a: Asset, duration: number) {
-	  if (!isPause.current) {
-		if (!a.directions || a.directions.distance === 0) {
-		  a.destinationSpace = mapData.getByType("space")[endPosition];
-		  a.directions = mapView.getDirections(a.originSpace, a.destinationSpace);
-		  mapView.Navigation.draw(a.directions)
-		  if (a.directions && a.directions.distance === 0) {
-			return;
-		  }
-		}
+        setCurrentDistance(`${distance} meters`);
 
-		if (a.directions?.instructions) {
-			console.log(a.directions?.instructions); // Log the instructions for debugging
-			// Call the function to update Firestore with the instructions
-			updateFirestoreWithInstructions(a.directions.instructions);
-		  }
-		const coordinate = a.directions?.coordinates[a.step];
-		const distance = roundDistance(
-		  coordinate?.latitude,
-		  coordinate?.longitude,
-		  a.currentInstruction?.coordinate.latitude,
-		  a.currentInstruction?.coordinate.longitude
-		);
+        const foundInstruction = a.directions?.instructions.find(
+          (instr) =>
+            instr.coordinate.latitude === coordinate?.latitude &&
+            instr.coordinate.longitude === coordinate?.longitude
+        );
+        const instruction = foundInstruction || a.currentInstruction;
 
-		setCurrentDistance(`${distance} meters`);
+        if (!coordinate || !instruction) return;
 
-		const foundInstruction = a.directions?.instructions.find(
-		  (instr) =>
-			instr.coordinate.latitude === coordinate?.latitude &&
-			instr.coordinate.longitude === coordinate?.longitude
-		);
-		const instruction = foundInstruction || a.currentInstruction;
+        if (foundInstruction) {
+          updateFirestoreWithInstructions(instruction);
+          findNearbyPOIs()
+          setTranscription(
+            `${instruction.action.type} ${
+              instruction.action.bearing ?? ""
+            } and walk ${Math.round(distance / 0.7)} steps`
+          );
+          a.currentInstruction = instruction;
+        }
 
-		if (!coordinate || !instruction) return;
+        if (a.marker) {
+          mapView.Markers.animateTo(a.marker, coordinate, {
+            duration: duration,
+            easing: "linear",
+          });
 
-		if (foundInstruction) {
-		  setTranscription(
-			`${instruction.action.type} ${
-			  instruction.action.bearing ?? ""
-			} and walk ${Math.round(distance/0.7)} steps`
-		  );
-		  a.currentInstruction = instruction;
-		}
+          setCurrentCoordinates({
+            lat: coordinate.latitude,
+            lon: coordinate.longitude,
+          });
+        }
 
-		if (a.marker) {
-		  mapView.Markers.animateTo(a.marker, coordinate, {
-			duration: duration,
-			easing: "linear",
-		  });
+        if (a.directions && a.step >= a.directions.coordinates.length - 1) {
+          setTranscription("Arrived");
+          setPaused(true); // Set paused to true when arrived
+          startPosition = endPosition; // Update the start position to current end position
+          endPosition = 13;
+          isPause.current = true;
+          a.directions = undefined;
+          a.step = 0; // Reset step
+        } else {
+          a.step += 1;
+        }
+      }
+     
+    }
 
-		  setCurrentCoordinates({
-			lat: coordinate.latitude,
-			lon: coordinate.longitude,
-		  });
-		}
+    function addAsset(name: string, image: string, color: string = "#0279FF") {
+      const originSpace = mapData.getByType("space")[startPosition];
+      const marker = mapView.Markers.add(
+        originSpace,
+        createGuardMarker(color, name, image),
+        { rank: "always-visible" }
+      );
 
-		if (a.directions && a.step >= a.directions.coordinates.length - 1) {
-		  setTranscription("Arrived");
-		  setPaused(true); // Set paused to true when arrived
-		  startPosition = endPosition; // Update the start position to current end position
-		  endPosition = 13
-		  isPause.current = true;
-		  a.directions = undefined
-		  a.step = 0; // Reset step
-		} else {
-		  a.step += 1;
-		}
-	  }
-	}
+      assets.push({
+        name: name,
+        image: image,
+        color: color,
+        originSpace: originSpace,
+        marker: marker,
+        step: 0,
+        currentInstruction: null,
+      });
+    }
 
-	function addAsset(name: string, image: string, color: string = "#0279FF") {
-	  const originSpace = mapData.getByType("space")[startPosition];
-	  const marker = mapView.Markers.add(
-		originSpace,
-		createGuardMarker(color, name, image),
-		{ rank: "always-visible" }
-	  );
+    addAsset("John", "");
 
-	  assets.push({
-		name: name,
-		image: image,
-		color: color,
-		originSpace: originSpace,
-		marker: marker,
-		step: 0,
-		currentInstruction: null,
-	  });
-	}
+    function updatePositions() {
+      updateAsset(assets[0], 1000);
+      setTimeout(updatePositions, 1500);
+    }
 
-	addAsset("John", "");
-
-	function updatePositions() {
-	  updateAsset(assets[0], 1000);
-	  setTimeout(updatePositions, 1500);
-	}
-
-	updatePositions();
+    updatePositions();
   }
-
 
   function calculateBearing(
     lat1: number,
@@ -307,30 +338,32 @@ const Page = () => {
 
       // Set the closest POIs to state
       setNearbyPOIs(closestPOIs);
+      await updateFirestoreWithNearme(closestPOIs)
     } else {
       console.error("Current coordinates not available.");
     }
   }
 
   useEffect(() => {
-    init();
-  }, []);
+    init(data);
+  }, [data]);
 
   return (
     <>
-
       <div ref={mapRef} id="mappedin-map" className="w-screen h-screen"></div>
-		<div  className="fixed top-4 left-4" >
-		{transcription && (
+      <div className="fixed top-4 left-4">
+        {transcription && (
           <div className="mt-4 p-2 bg-white rounded-2xl	">
             <p className="text-2xl	">
               <strong>{transcription}</strong>
             </p>
           </div>
         )}
-		{nearbyPOIs.length > 0 && (
+        {nearbyPOIs.length > 0 && (
           <div className="mt-4 p-2 bg-white rounded-xl">
-            <p><strong>Your are near:</strong></p>
+            <p>
+              <strong>Your are near:</strong>
+            </p>
             <ul>
               {nearbyPOIs.map((poi, index) => (
                 <li key={index}>{poi}</li>
@@ -338,43 +371,41 @@ const Page = () => {
             </ul>
           </div>
         )}
-		</div>
+      </div>
       <div className="fixed bottom-4 left-4">
-	  <button
-      className="bg-blue-800 rounded-full text-white p-4 mt-4 mx-2 w-3/6"
-      onClick={() => {
-        isPause.current = !isPause.current;
-        setPaused(!paused);
-		if (transcription === "Arrived") {
-			setTranscription("");  // Reset the transcription state
-			init();           // Call the init function
-		  }
-
-      }}
-    >
-      {paused ? (
-        <FontAwesomeIcon icon={faPlay} />
-      ) : (
-        <FontAwesomeIcon icon={faPause} />
-      )}
-    </button>
-	<div className="flex row">
-		<div className="mt-4 p-2 bg-white rounded-xl">
-        <h1><strong>Destinaton Address:</strong></h1>
-        <pre>{JSON.stringify(data.current, null, 2)}</pre>
-		</div>
-
-
-        {/* Button to find nearby POIs */}
         <button
-          className="bg-green-600 rounded-xl ml-2 text-white p-2 mt-4 pointer "
-          onClick={findNearbyPOIs}
+          className="bg-blue-800 rounded-full text-white p-4 mt-4 mx-2 w-3/6"
+          onClick={() => {
+            isPause.current = !isPause.current;
+            setPaused(!paused);
+            if (transcription === "Arrived") {
+              setTranscription(""); // Reset the transcription state
+              init(); // Call the init function
+            }
+          }}
         >
-          Where am I?
+          {paused ? (
+            <FontAwesomeIcon icon={faPlay} />
+          ) : (
+            <FontAwesomeIcon icon={faPause} />
+          )}
         </button>
-		</div>
+        <div className="flex row">
+          <div className="mt-4 p-2 bg-white rounded-xl">
+            <h1>
+              <strong>Destinaton Address:</strong>
+            </h1>
+            <pre>{data}</pre>
+          </div>
 
-
+          {/* Button to find nearby POIs */}
+          <button
+            className="bg-green-600 rounded-xl ml-2 text-white p-2 mt-4 pointer "
+            onClick={findNearbyPOIs}
+          >
+            Where am I?
+          </button>
+        </div>
       </div>
     </>
   );
